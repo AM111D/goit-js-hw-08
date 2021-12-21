1e4934ba711036e8e5bd8e28592569851d543a13
AM111D <Dimasssnike@icloud.com>
2021-12-21 21:56:59 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea')
+}
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(evt) {}
+
+function onTextareaInput(evt) {
+    const message = evt.currentTarget.value;
+    localStorage.setItem('feedback-form-state', message);
+    console.log(message);
+}
+
+function populateMessageOutput() {}
+
+
+
+
+
+
+
+
 
