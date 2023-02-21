472c0055424f2380e0fc4684125088ee0a28cbf1
AM111D <Dimasssnike@icloud.com>
2023-02-21 10:06:11 -0800

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,16 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
+player.on('play', function () {
+  console.log('played the video!');
 });
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
 });
 
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
 };
 
 player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+// --------------------------IMPORTS---------------------------------------
+import throttle from 'lodash.throttle';
 
+// --------------------------EXPORT-----------------------------------------
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+// --------------------------FUNCTIONS-------------------------------------
+
+form.addEventListener('submit', onFromSubmit);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onFromSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(event) {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(formData);
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+populateTextarea();
+
+function populateTextarea() {
+  const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (saveData) {
+    email.value = saveData.email;
+    message.value = saveData.message;
+  }
+}
